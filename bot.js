0b67d043c09852318fae5401d518ad1e6645ba7f
3RBTNTxb3 <42219832+3RBTNTxb3@users.noreply.github.com>
2018-08-09 10:29:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
-constclient.on('ready', () => {
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`<help>`,"<help>")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
